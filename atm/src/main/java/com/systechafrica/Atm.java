074eb7028266672796a976a03df116f8ac138c23
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-08-30 11:10:21 +0300

    fixed the counter issue

--- atm/src/main/java/com/systechafrica/Atm.java
+++ atm/src/main/java/com/systechafrica/Atm.java
@@ -2,34 +2,55 @@ package com.systechafrica;
 
 import java.util.Scanner;
 
-public class Atm extends Thread {
+public class Atm {
 
-   static Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
 
     static int originalBalance = 10001000;
 
-static void logIn(int customerNumber,String pass){
-    int count = 3;
+    static int customerInput;
+    static String suppliedPass;
 
-    
-if (customerNumber == 10 && pass.equals("Admin123")) {
-    displayMenu();
-}else{
+    static int count = 3;
 
-count --;
-if(count !=0){
-System.out.println("INVALID CREDENTIALS!!");
-System.out.println(count+" attempts remaining!!");
+    static void startPrompts(int counter) {
+        if (counter > 0) {
+            System.out.println("Enter customer number : ");
+            customerInput = scanner.nextInt();
 
-}else{
-    System.out.println("OOps!! you are out of attempts try again in 10 minutes!!!");
-}
+            scanner.nextLine();
 
-}
+            System.out.println("Enter customer password : ");
+            suppliedPass = scanner.nextLine();
 
+            logIn(customerInput, suppliedPass);
 
+        } else {
+            System.out.println("OOps!!");
+        }
 
-}
+    }
+
+    static int logIn(int customerNumber, String pass) {
+
+        if (customerNumber == 10 && pass.equals("Admin123")) {
+            displayMenu();
+        } else {
+            count--;
+            // if(count !=0){
+            System.out.println("INVALID CREDENTIALS!!");
+            System.out.println(count + " attempts remaining!!");
+            startPrompts(count);
+
+            // }else{
+            // System.out.println("OOps!! you are out of attempts try again in 10
+            // minutes!!!");
+            // }
+
+        }
+
+        return count;
+    }
 
     static void displayMenu() {
         System.out.println("you are in the menu");
@@ -66,9 +87,6 @@ System.out.println(count+" attempts remaining!!");
     }
 
     public static void main(String[] args) {
-        
-
-
 
         System.out.println("Welcome to ATM SIMULATOR, press Y to continue");
 
@@ -76,19 +94,11 @@ System.out.println(count+" attempts remaining!!");
 
         if (cont.matches("y")) {
 
+            // logIn(customerInput, suppliedPass);
+            startPrompts(count);
 
-System.out.println("Enter customer number : ");
-int customerInput = scanner.nextInt();
-
-scanner.nextLine();
-
-System.out.println("Enter customer password : ");
-String suppliedPass = scanner.nextLine();
-
-logIn(customerInput, suppliedPass);
-
-        }else{
-        System.out.println("You didnt press 'y' to continue");
+        } else {
+            System.out.println("You didnt press 'y' to continue");
         }
     }
 }
