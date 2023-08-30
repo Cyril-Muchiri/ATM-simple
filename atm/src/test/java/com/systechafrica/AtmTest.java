95b5739db1096f3ec638d0d0a093afe47263c7e5
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-08-30 22:30:02 +0300

    added some tests

--- atm/src/test/java/com/systechafrica/AtmTest.java
+++ atm/src/test/java/com/systechafrica/AtmTest.java
@@ -1,15 +1,28 @@
 package com.systechafrica;
 
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-/**
- * Unit test for simple App.
- */
 public class AtmTest {
-
+    int count = 3;
+Atm atm =new Atm();
     @Test
     void objectExists(){
+Assertions.assertNotNull(atm,"should fail if object is not created and is empty");
+    }
+
+    @Test
+    void ifThreadAlive(){
+        
+        Assertions.assertTrue(!atm.isAlive(),"Test whether the thread is alive");
+    }
+
+    @Test
+    void loginThrows(){
+        
+        int expectedCustomerInput = 5;
+        String expectedPass = "Admin444";
+       
+
 
     }
 
